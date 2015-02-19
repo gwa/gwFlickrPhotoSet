badb3b9cb38ee9a6e7886f84e42525976187842e
prisis <info@anolilab.de>
2015-02-20 00:25:44 +0100

    Added config for travis

---
+++ tests/flickrConfig.php
@@ -0,0 +1,8 @@
+<?php
+return array(
+    'idphotoset' => '72157624618609504',
+    'flickrapikey' => getenv('KEY'),
+
+    /*Flicker data for exception test*/
+    'idphotoset2' => '1487039626820',
+);
